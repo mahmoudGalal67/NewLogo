4d192d2a8ae120f1db06318b9a12bc6fc8a59f0f
mahmoudGalal67 <mahmoudfarag270@gmail.com>
2025-09-21 09:21:26 +0300

    fdss

--- src/pages/ProjectDetails.jsx
+++ src/pages/ProjectDetails.jsx
@@ -1,19 +1,34 @@
-import React from "react";
+import { useGSAP } from "@gsap/react";
+import gsap from "gsap";
 import { useParams } from "react-router-dom";
 
 function ProjectDetails() {
   const { id } = useParams();
 
+  useGSAP(() => {
+    const ProjectDetails = document.querySelector("#ProjectDetails");
+
+    gsap.fromTo(
+      ProjectDetails,
+      { autoAlpha: 0, y: "180%" },
+      {
+        autoAlpha: 1,
+        y: 0,
+        duration: 1.5,
+      }
+    );
+  }, []);
+
   return (
     <div className="project-details">
-      <section className="banner mt-[180px] p-12 rounded-[86px] bg-[#EBEBEB]">
+      <section className="banner md:mt-[180px] mt-[120px] md:p-12 p-2 md:h-[auto] h-[180px]  md:rounded-[86px] rounded-2xl bg-[#EBEBEB]">
         <img
           src={`/projects/project${id}.jpg`}
-          className="w-full h-[340px] object-cover  rounded-[86px]"
+          className="w-full md:h-[340px] h-full object-cover  md:rounded-[86px] rounded-2xl"
           alt=""
         />
       </section>
-      <div className="content mt-[140px]">
+      <div id="ProjectDetails" className="content md:mt-[140px] mt-[45px]">
         <img src="/pattern.png" className="w-full" alt="" />
       </div>
     </div>
